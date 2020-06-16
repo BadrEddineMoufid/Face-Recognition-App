a7956138ceb5331343d03143ca02fc8a3afec653
Badr Eddine <bin.benh@gmail.com>
2020-06-16 15:02:33 +0100

    fixed a typo

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -82,7 +82,7 @@ class Register extends React.Component {
                             <input
                                 className="" 
                                 onClick={this.onSubmitSignIn}
-                                class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                                 type="submit" 
                                 value="Register" />
                         </div>
